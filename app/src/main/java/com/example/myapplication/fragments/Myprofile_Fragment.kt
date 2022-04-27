e3ce0519b1ddcacec4bc4fd06db76ac4e4e04fc0
user2 <diana.rtveladze.1@btu.edu.ge>
2022-04-27 23:07:01 +0400

    commit

---
+++ app/src/main/java/com/example/myapplication/fragments/Myprofile_Fragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class Myprofile_Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_myprofile, container, true)
+        return view
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/fragments/Notification_Fragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class Notification_Fragment: Fragment(R.layout.fragment_notification){
+}
